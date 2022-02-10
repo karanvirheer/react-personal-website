11adb59483b129f41e0395b04168dbe3fb529d18
Karanvir Heer <karanvirheer@live.com>
2022-02-09 22:15:08 -0500

    npm build finished

---
+++ build/precache-manifest.d174223e8bedeb7181ca9cf5b306ffc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9872f6cf9a7c08422d2f11c21c7fb33c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d957964affe4c5dc3ff",
+    "url": "/static/css/main.8fb17762.chunk.css"
+  },
+  {
+    "revision": "026f55c65ba38955cff7",
+    "url": "/static/js/2.afc119b4.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/static/js/2.afc119b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d957964affe4c5dc3ff",
+    "url": "/static/js/main.ca45c402.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
